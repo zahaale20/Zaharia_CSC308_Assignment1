133ca5336bbdc7ed50ccb18a25fb1bf39c2f35ff
Alex Zaharia <alexcz@outlook.com>
2024-01-10 15:53:20 -0800

    finished assignment 1

--- react-assignment1/src/App.js
+++ react-assignment1/src/App.js
@@ -1,36 +1,25 @@
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 function App() {
-  const [characters, setCharacters] = useState([ 
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
-       return i !== index
+        return i !== index
     });
     setCharacters(updated);
- }
+  }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
 
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
     </div>
   );
 
